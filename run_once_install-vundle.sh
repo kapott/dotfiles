1d81be3c1192cff03a465482b5d251682ced81e8
kapott <github@aivd.33mail.com>
2026-02-04 14:25:02 +0100

    Always run the plugin install

--- run_once_install-vundle.sh
+++ run_once_install-vundle.sh
@@ -5,8 +5,11 @@ VUNDLE_DIR="${HOME}/.config/vim/bundle/Vundle.vim"
 if [ ! -d "$VUNDLE_DIR" ]; then
     echo "Installing Vundle..."
     git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
-    vim +PluginInstall +qall
     echo "Vundle installed and plugins synced."
 else
     echo "Vundle already installed."
 fi
+
+echo "Installing Vim plugins.."
+vim +PluginInstall +qall
+echo "Vundle and plugins synced."
